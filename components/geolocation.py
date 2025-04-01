ca14e56866c42c8e814a67f4d1659d05948fb8ab
Shubham Das <134552476+SENSEi-777@users.noreply.github.com>
2025-04-02 01:48:56 +0530

    Delete components/geolocation.py

--- components/geolocation.py
+++
@@ -1,6 +0,0 @@
-import streamlit.components.v1 as components
-def get_geolocation():
-    return components.declare_component(
-        "geolocation",
-        url="https://asddetection-webapp.streamlit.app/"  # For production
-    )
