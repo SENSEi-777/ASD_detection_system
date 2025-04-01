b66fbd706b4358ff870b9db31ebfe73bd2582788
Shubham Das <134552476+SENSEi-777@users.noreply.github.com>
2025-04-02 02:17:40 +0530

    Delete components/geolocation.py

--- components/geolocation.py
+++
@@ -1,7 +0,0 @@
-import streamlit.components.v1 as components
-
-def get_geolocation():
-    return components.declare_component(
-        "geolocation",
-        path="./frontend/build"  # Point to built files
-    )
